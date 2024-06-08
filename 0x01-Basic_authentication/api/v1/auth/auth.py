7a4fb9080d78a0a8e8128561d6d4032834c2aff1
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-06-08 05:19:43 -0700

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,8 +2,7 @@
 """
 This  module contains the auth class
 """
-
-from flask import  requets
+from flask import request
 from typing import List, TypeVar
 
 
@@ -12,12 +11,40 @@ clas Auth:
     Manages the Authentication API
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        """
+        Determines if a given path is authorized or not
+        """
 
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[:-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        return None
-
+        """
+        Returns the authorization header for a request object
+        """
+        if request is None:
+            return None
+        header = request.headers.get("Authorization")
+        if header is None:
+            return None
+        return header
 
     def current_user(self, request=None) -> TypeVar("User"):
+        """
+        Returns a user instance form information from the request header
+        """
         return None
