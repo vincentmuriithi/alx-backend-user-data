c91bea7c916b95599fc6a9e0d539d154ada65cce
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-06-08 13:32:13 +0300

    .\auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This  module contains the autj class
+This  module contains the auth class
 """
 
 from flask import  requets
@@ -8,4 +8,16 @@ from typing import List, TypeVar
 
 
 clas Auth:
-    def 
+    """
+    Manages the Authentication API
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        return None
